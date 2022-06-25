501557ac48bb82e5d50cb1b3c772da18e929233c
Julien Rolland <rollandjulien13013@gmail.com>
2022-06-25 14:50:42 +0200

    apikey setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :api_keys, only: %i[create destroy index]
   namespace :api do
     namespace :v1 do
       get '/longest_words/top', to: 'longest_words#top'
